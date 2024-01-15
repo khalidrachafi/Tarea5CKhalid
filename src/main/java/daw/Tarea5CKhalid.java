c055b91cf87886f2b8755076e6b78c4df5faff8a
kracelh542 <kracelh542@gmail.com>
2024-01-15 09:27:05 +0100

    creacion proyecto

---
+++ src/main/java/daw/Tarea5CKhalid.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author khalid
+ */
+public class Tarea5CKhalid {
+
+
+     public String nombre;
+     public double num;
+     
+     
+
+
+
+
+
+    
+}
